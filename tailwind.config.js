7bf8ebd7c9ec2a9293a07b203eb19a347a7cc919
AMP3D <ampeiravi+github@gmail.com>
2024-05-03 14:36:30 -0700

    Update colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   daisyui: {
-    themes: ["night"],
+    themes: [
+      {
+        mytheme: {
+          primary: "#065f46",
+          secondary: "#042f2e",
+          "secondary-content": "#047857",
+          accent: "#66ffdb",
+          neutral: "#0f766e",
+          "base-100": "#b9bdb5",
+          "base-content": "#66ffdb",
+          info: "#8c7501",
+          success: "#021f1a",
+          warning: "#c94902",
+          error: "#ba0000",
+          disabled: "#ba0000",
+        },
+      },
+    ],
   },
   theme: {
     extend: {},
