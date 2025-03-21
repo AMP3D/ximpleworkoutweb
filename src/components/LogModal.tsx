6afa143dcab20ff2337c127eeedfa698407c51f0
Arman <armanp@interknowlogy.com>
2025-03-20 19:53:42 -0700

    Sort log section, touch up styling, and reduce container height.

--- src/components/LogModal.tsx
+++ src/components/LogModal.tsx
@@ -14,13 +14,20 @@ const LogModal: FC<LogModalProps> = (props: LogModalProps) => {
   );
 
   // Filter entries for selected date
-  const filteredEntries = Object.entries(lastCompletedSets).filter(
-    /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
-    ([_, entry]) => {
-      const entryDate = dayjs(entry).format("YYYY-MM-DD");
-      return entryDate === selectedDate;
-    }
-  );
+  const filteredEntries = Object.entries(lastCompletedSets)
+    .filter(
+      /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
+      ([_, entry]) => {
+        const entryDate = dayjs(entry).format("YYYY-MM-DD");
+        return entryDate === selectedDate;
+      }
+    )
+    .sort((a, b) => {
+      const dateA = dayjs(a[1]);
+      const dateB = dayjs(b[1]);
+
+      return dateA.isBefore(dateB) ? -1 : 1;
+    });
 
   return (
     <Modal onModalClose={props.onModalClose} preventDefault={true} title="Log">
@@ -28,33 +35,35 @@ const LogModal: FC<LogModalProps> = (props: LogModalProps) => {
         <div className="flex justify-center">
           <input
             type="date"
-            className="input input-bordered w-full max-w-xs bg-secondary text-primary-content"
+            className="input input-bordered w-full max-w-xs bg-secondary text-primary-content [color-scheme:dark]"
             value={selectedDate}
             onChange={(e) => setSelectedDate(e.target.value)}
           />
         </div>
 
-        <div className="overflow-x-auto">
-          <table className="table w-full">
-            <thead>
-              <tr className="bg-secondary text-primary-content ">
-                <th className="text-base">Exercise</th>
-                <th className="text-base">Details</th>
-              </tr>
-            </thead>
-            <tbody>
-              {filteredEntries.map(([key, entry], index) => (
-                <tr
-                  key={index}
-                  className={index % 2 === 0 ? "bg-primary" : "bg-secondary"}
-                >
-                  <td className="font-medium">{key}</td>
-                  <td>{entry}</td>
+        {!!filteredEntries.length && (
+          <div className="overflow-x-auto max-h-[calc(60vh)] overflow-y-auto">
+            <table className="table w-full">
+              <thead>
+                <tr className="bg-secondary text-primary-content ">
+                  <th className="text-base">Exercise</th>
+                  <th className="text-base">Details</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+              </thead>
+              <tbody>
+                {filteredEntries.map(([key, entry], index) => (
+                  <tr
+                    key={index}
+                    className={index % 2 === 0 ? "bg-primary" : "bg-secondary"}
+                  >
+                    <td className="font-medium">{key}</td>
+                    <td>{entry}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
       </div>
     </Modal>
   );
