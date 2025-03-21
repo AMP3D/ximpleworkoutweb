8fad76c8912c6a06aaff6d42c27db5221e878fe3
Arman <armanp@interknowlogy.com>
2025-03-20 19:39:54 -0700

    Fix linting issue

--- src/components/LogModal.tsx
+++ src/components/LogModal.tsx
@@ -14,12 +14,12 @@ const LogModal: FC<LogModalProps> = (props: LogModalProps) => {
   );
 
   // Filter entries for selected date
-  const filteredEntries = Object.entries(lastCompletedSets).filter(
-    ([_, entry]) => {
-      const entryDate = dayjs(entry).format("YYYY-MM-DD");
-      return entryDate === selectedDate;
-    }
-  );
+  const filteredEntries = Object.keys(lastCompletedSets).filter((key) => {
+    const entry = lastCompletedSets[key];
+    const entryDate = dayjs(entry).format("YYYY-MM-DD");
+
+    return entryDate === selectedDate;
+  });
 
   return (
     <Modal onModalClose={props.onModalClose} preventDefault={true} title="Log">
